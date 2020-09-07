41eb144fc42207a898d371b3f7bc96687eaa4438
itrover <1172610139@qq.com>
2020-09-07 18:53:25 +0800

    update：数字序列中某一位的数字-findNthDigit(找规律)

--- src/swordToOffer/findNthDigit/Solution.java
+++ src/swordToOffer/findNthDigit/Solution.java
@@ -1,20 +1,27 @@
 package swordToOffer.findNthDigit;
 
 /**
+ * 剑指 Offer 44. 数字序列中某一位的数字
  * 找规律的题
  */
 class Solution {
     public int findNthDigit(int n) {
+        // 区间位数
         int digit = 1;
+        // 区间开始的数字
         long start = 1;
+        // 每个区间对应的个数
         long count = 9;
+        // 找到数字对应的区间
         while (n > count) {
             n -= count;
             digit += 1;
             start *= 10;
             count = digit * start * 9;
         }
+        // 找到数字
         long num = start + (n - 1) / digit;
+        // 定位数字的位
         return Long.toString(num).charAt((n - 1) % digit) - '0';
     }
 }
