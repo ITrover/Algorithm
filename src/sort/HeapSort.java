b0e2118b20723c2ff7f515930887583f88449b92
itrover <1172610139@qq.com>
2020-11-18 18:29:02 +0800

    update:Heapsort

--- src/sort/HeapSort.java
+++ src/sort/HeapSort.java
@@ -3,22 +3,25 @@ package sort;
 /**
  * 堆排序
  * 基本思路
- *1.将待排序集合，用堆表示，将最大的数放在堆顶。
- *2.将堆顶元素和堆尾元素交换，同时堆的长度-1
- *重复1 2 操作直到堆的长度变为1.
+ * 1.将待排序集合，用堆表示，将最大的数放在堆顶。
+ * 2.将堆顶元素和堆尾元素交换，同时堆的长度-1
+ * 重复1 2 操作直到堆的长度变为1.
+ *
  * @param <T>
  */
 public class HeapSort<T extends Comparable> {
     public void sort(T[] t) {
         int heaplen = t.length;
         for (int i = t.length - 1; i > 0; i--) {
-            buildHeap(t, heaplen );
+            buildHeap(t, heaplen);
             swap(t, 0, i);
-            heaplen -- ;
+            heaplen--;
         }
     }
+
     /**
      * 调整堆，让i位置的节点的子节点小于自身。
+     *
      * @param t   堆
      * @param i   根节点
      * @param len 对的长度
@@ -38,8 +41,10 @@ public class HeapSort<T extends Comparable> {
             swap(t, i, largest);
         }
     }
+
     /**
      * 经过调整，将最大的元素放在第一个位置（0）
+     *
      * @param t
      * @param len logical length of t
      */
@@ -49,6 +54,7 @@ public class HeapSort<T extends Comparable> {
             adjustHeap(t, i, len);
         }
     }
+
     private void swap(T[] t, int i, int largest) {
         T tmp = t[i];
         t[i] = t[largest];
