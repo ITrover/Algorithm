b935f64e7ea71b70fbdca946725c41bf88ad3069
itrover <1172610139@qq.com>
2020-11-28 11:40:35 +0800

    add:剑指 Offer 14- I. 剪绳子(cuttingRope)-数学推导、贪心算法、循环求余

--- src/swordToOffer/cuttingRope/Solution.java
+++ src/swordToOffer/cuttingRope/Solution.java
@@ -4,9 +4,13 @@ package swordToOffer.cuttingRope;
  * @author MaYunHao
  * @version 1.0
  * @date 2020/9/4 9:21
- * @description 剑指 Offer 14- I. 剪绳子
+ * @description 剑指 Offer 14- I. 剪绳子 https://leetcode-cn.com/problems/jian-sheng-zi-lcof/
  * 数学推导
- *
+ * 尽量把绳子分成多个长度为 3 的片段
+ * 分情况对不能分成3的片段处理
+ * 0 不用处理
+ * 1 将一个3的片段分解，因为2x2 > 3x1
+ * 2 与3的片段相乘后返回
  */
 public class Solution {
     public int cuttingRope(int n) {
