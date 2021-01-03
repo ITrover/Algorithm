4b05ab97fa65f15b57542f369681c2672e1b9bf8
itrover <1172610139@qq.com>
2021-01-03 20:44:15 +0800

    add:86. 分隔链表(partition)-双指针

--- src/everyday/partition/Solution.java
+++ src/everyday/partition/Solution.java
@@ -4,6 +4,8 @@ package everyday.partition;
  * @author itrover
  * 86. 分隔链表 https://leetcode-cn.com/problems/partition-list/
  * 双指针
+ * 大指针一直往前移动，一旦遇到了小于x的，则插入到小指针的末尾
+ * 方法二：分别用两个链表保持两段(大的和小的)
  */
 class Solution {
     public class ListNode {
@@ -23,7 +25,8 @@ class Solution {
         while (big != null && big.next != null) {
             // 如果小于了x，则将该结点移动到小指针的后面
             if (big.next.val < x) {
-                // 如果相等，则一起跳过
+                // 如果相等，则一起跳过，避免big和small指向同一个结点时，出现死循环
+                // 即big先截断node，small又把node添加到了指针后面，big无法往前移动
                 if (big == small){
                     big = big.next;
                 }else{
