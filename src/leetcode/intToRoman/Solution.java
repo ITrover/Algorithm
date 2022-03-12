963b5385ece3611385cb9319605b323ad0b2c0a7
itrover <1172610139@qq.com>
2022-03-12 13:25:05 +0800

    add:12. 整数转罗马数字(intToRoman)-枚举

---
+++ src/leetcode/intToRoman/Solution.java
@@ -0,0 +1,23 @@
+package leetcode.intToRoman;
+
+/**
+ * @author itrover
+ * 12. 整数转罗马数字 https://leetcode-cn.com/problems/integer-to-roman/
+ * 枚举
+ */
+class Solution {
+    public String intToRoman(int num) {
+        int[] base = new int[]{1, 4, 5, 9, 10, 40, 50, 90, 100, 400, 500, 900, 1000};
+        String[] s = new String[]{"I", "IV", "V", "IX", "X", "XL", "L", "XC", "C", "CD", "D", "CM", "M"};
+        StringBuilder sb = new StringBuilder();
+        // 枚举每一位
+        for (int i = base.length - 1; i >= 0; i--) {
+            while (num >= base[i]) {
+                sb.append(s[i]);
+                num -= base[i];
+            }
+        }
+        // 压缩,寻找连续的I,X,C
+        return sb.toString();
+    }
+}
